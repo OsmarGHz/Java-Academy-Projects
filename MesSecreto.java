9554abfbaea24060de0efff7ab72552b379f1c50
OsmarGHz <osmarjavier1@gmail.com>
2024-10-01 23:27:42 -0600

    Added solution to secretMonth (name in english)

--- MesSecreto.java
+++ MesSecreto.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class MesSecreto{
-    
+
+    public static int adivinador(String respuesta, String mesSecreto){
+        if (!mesSecreto.equals(respuesta)) {
+            System.out.print("\nRespuesta incorrecta. Intente de nuevo con otro mes: ");
+            return 0;
+        }else{
+            System.out.print("\n¡Lo has logrado! ");
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        String meses [] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        String mesSecreto = meses[2], respuesta;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("\nTrate de adivinar el mes secreto, ingresando el nombre del mes en minúsculas: ");
+        do { respuesta = entrada.next(); } while (adivinador(respuesta,mesSecreto)==0);
+        entrada.close();
+    }
 }
